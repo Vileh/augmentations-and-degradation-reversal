ff4d07ab2132962a252911151da5b7bcdf361f66
Viktor Penchev <victorpenchev277@gmail.com>
2024-11-02 18:32:18 +0000

    Update download_data.py

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -13,7 +13,7 @@ wildlife_datasets = [
     "LeopardID2022",
     "MacaqueFaces",
     "NyalaData",
-    "SealID",
+    # "SealID", # Requires URL
     "SeaTurtleIDHeads",
     # "StripeSpotter", # Downloaded externally
     "WhaleSharkID",
@@ -23,7 +23,4 @@ wildlife_datasets = [
 for dataset in wildlife_datasets:
     dataset_func = getattr(datasets, dataset)
 
-    if dataset == "SealID":
-        dataset_func.get_data(f'../datasets/{dataset}', url="https://download.fairdata.fi:443/download?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MzAyMDM2MzMsImRhdGFzZXQiOiIyMmI1MTkxZS1mMjRiLTQ0NTctOTNkMy05NTc5N2M5MDBmYzAiLCJwYWNrYWdlIjoiMjJiNTE5MWUtZjI0Yi00NDU3LTkzZDMtOTU3OTdjOTAwZmMwXzVxcmNrZ290LnppcCIsImdlbmVyYXRlZF9ieSI6ImE0MzdhOGRmLTllZjEtNDVjOC1hYWQxLTQ4MmYxYTA4ZTM1YiIsInJhbmRvbV9zYWx0IjoiMjhjYjE1MzYifQ.HzPaBTEoa8Scw5f9pdGicMFJXvsvkvdGg8r_y3fRawo")
-    else:
-        dataset_func.get_data(f'../datasets/{dataset}')
+    dataset_func.get_data(f'../datasets/{dataset}')
